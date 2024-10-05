6de0454cd6ab1d950c33a336ea358af29659ef92
Кира Белашова <KV.Belashova@gmail.com>
2024-10-05 09:11:10 +0300

    file changed

---
+++ timer.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var year := readinteger('Введите год:');
+  if (year mod 4 = 0) then
+  begin
+    if (year mod 100 = 0) and (year mod 400 <> 0) then
+      println('Не високосный год') 
+    else
+      println('Високосный год')
+  end
+  else
+    println('Не високосный год');
+end.
