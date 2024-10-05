d6b7fa4602d0c4870c308f993be8f4a33b81a925
Кира Белашова <KV.Belashova@gmail.com>
2024-10-05 09:22:37 +0300

    task3

---
+++ time.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var h := readinteger('Введите час:');
+  var m := readinteger('Введите минуту:');
+  var s := readinteger('Введите секунду:');
+  var sum: integer;
+  sum := h * 3600 + m * 60 + s;
+  println(sum);
+end.
--- timer.pas
+++ timer.pas
@@ -3,10 +3,10 @@
   if (year mod 4 = 0) then
   begin
     if (year mod 100 = 0) and (year mod 400 <> 0) then
-      println('Не високосный год') 
+      println('365') 
     else
-      println('Високосный год')
+      println('366')
   end
   else
-    println('Не високосный год');
+    println('365');
 end.
