061f083f018d17ec0ed26ad721ab2c9641d34a22
Кира Белашова <KV.Belashova@gmail.com>
2024-10-05 08:53:35 +0300

    file changed

--- hello.pas
+++ hello.pas
@@ -1,4 +1,11 @@
 ﻿begin
-  Print('Привет мир!');
-  
+  var hour := readinteger('Введите час:');
+  var hi : string;
+  case hour of
+    4..10: hi := 'Доброе утро, мир!';
+    11..16: hi := 'Добрый день, мир!';
+    17..22: hi := 'Добрый вечер, мир!';
+  else hi := 'Доброй ночи, мир!';
+  end;
+  println(hi);
 end.
